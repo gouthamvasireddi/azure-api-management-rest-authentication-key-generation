31aed6bf3b9c032f92f25936bc5ac184974a9809
Goutham <goutham.vasireddi.osv@fedex.com>
2018-08-22 11:25:03 -0500

    Azure apim auth token generation sample

---
+++ AzureAPIUtil.java
@@ -0,0 +1,39 @@
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+
+
+public class AzureAPIUtil {
+
+	public static String buildAzureAuthorizationToken(String id, String secret) {
+		String base64Sign;
+
+		LocalDateTime date = LocalDateTime.now().plusDays(1);
+		String expiry = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:'00.0000000Z'"));
+		try {
+
+			String string_to_sign = id + "\n" + expiry;
+
+			Mac hmacSHA512 = Mac.getInstance("HmacSHA512");
+			SecretKeySpec secretKeySpec = new SecretKeySpec(secret.getBytes(), "HmacSHA512");
+			hmacSHA512.init(secretKeySpec);
+
+			byte[] digest = hmacSHA512.doFinal(string_to_sign.getBytes());
+
+			base64Sign = Base64.encodeBase64String(digest);
+
+		} catch (IllegalStateException | InvalidKeyException | NoSuchAlgorithmException ex) {
+			throw new RuntimeException("Problemas calculando HMAC", ex);
+		}
+
+		return "SharedAccessSignature "+id+"&"+date.format(DateTimeFormatter.ofPattern("yyyyMMddHHmm"))+"&"+base64Sign;
+	}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+Generating Authentication Key For Azure API Management in Java
+
+
+In extension to documentation in https://docs.microsoft.com/en-us/rest/api/apimanagement/apimanagementrest/azure-api-management-rest-api-authentication
+
+Azure does not have java documentation to generate the token so after a little struggle I was able to figure it out.
+
+Following java file has the sample code.
+
+Note: Two different date formats need to be used. 
+
+ 
+
